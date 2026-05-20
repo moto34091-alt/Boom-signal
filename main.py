c520c4fde04d7331ce9800c387f1a1fad58a3b04
Support Vimba boss <moto34091@gmail.com>
2026-05-20 23:31:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+import os
 
-TOKEN = "TON_TOKEN_ICI"
+TOKEN = os.getenv("TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "🚀 Scalping Bot Active"
-    )
+    await update.message.reply_text("🚀 Bot actif")
 
 
 app = Application.builder().token(TOKEN).build()
