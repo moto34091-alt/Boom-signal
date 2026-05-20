ecbe50652fbcb26a1e321fa2eeb5645e51b0590a
Support Vimba boss <moto34091@gmail.com>
2026-05-20 23:54:03 +0200

    Update market.py

--- market.py
+++ market.py
@@ -1,23 +1,15 @@
 import requests
 import pandas as pd
 
-API_KEY = "TON_API_KEY"
+API_KEY = "TON_TWELVEDATA_KEY"
 
-def get_candles(symbol="XAU/USD"):
+def get_data(symbol):
     url = f"https://api.twelvedata.com/time_series?symbol={symbol}&interval=1min&outputsize=50&apikey={API_KEY}"
-
     r = requests.get(url).json()
 
     data = r["values"]
-
     df = pd.DataFrame(data)
     df = df.astype(float)
 
-    df = df.rename(columns={
-        "open": "open",
-        "high": "high",
-        "low": "low",
-        "close": "close"
-    })
-
+    df = df[::-1]  # ordre chrono
     return df
