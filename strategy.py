3751883df2883529d3888337b071000d1b890c4c
Support Vimba boss <moto34091@gmail.com>
2026-05-21 01:06:28 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,11 +1,22 @@
+def price_changed(df):
+    last = df.iloc[-1]["close"]
+    prev = df.iloc[-2]["close"]
+
+    return abs(last - prev) > 0.0003
+
+
 def generate_signal(df):
 
     last = df.iloc[-1]
 
+    # 🔴 BLOQUE si pas de mouvement
+    if not price_changed(df):
+        return None
+
     score = 0
     direction = None
 
-    # trend EMA
+    # TREND
     if last["ema5"] > last["ema13"]:
         score += 30
         direction = "BUY"
