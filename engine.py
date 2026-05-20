8d716f68314b46be78da58685dc10a8463f1867e
Support Vimba boss <moto34091@gmail.com>
2026-05-21 00:07:43 +0200

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,29 +1,35 @@
 import time
+import asyncio
+
 from market import get_data
 from indicators import add_indicators
 from strategy import generate_signal
-from telegram_bot import send_signal  # 👈 AJOUT
+from telegram_bot import send_signal
+from main import running
 
 ASSETS = ["XAU/USD", "XAG/USD", "UKOIL", "USOIL"]
 
-CHAT_ID = "TON_CHAT_ID"  # 👈 ton Telegram ID
+CHAT_ID = "TON_CHAT_ID"
+
 
 def run():
+
     while True:
-        for asset in ASSETS:
 
-            df = get_data(asset)
-            df = add_indicators(df)
+        if running:
 
-            signal = generate_signal(df)
+            for asset in ASSETS:
 
-            if signal:
-                print(asset, signal)
+                df = get_data(asset)
+                df = add_indicators(df)
 
-                # 📩 ENVOI TELEGRAM
-                import asyncio
-                asyncio.run(
-                    send_signal(CHAT_ID, asset, signal)
-                )
+                signal = generate_signal(df)
+
+                if signal:
+                    print(asset, signal)
+
+                    asyncio.run(
+                        send_signal(CHAT_ID, asset, signal)
+                    )
 
         time.sleep(2)
