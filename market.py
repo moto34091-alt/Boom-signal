5802fd41a22466e0bbe0712b6e9128cd90313d07
Support Vimba boss <moto34091@gmail.com>
2026-05-21 09:21:04 +0200

    Update market.py

--- market.py
+++ market.py
@@ -4,47 +4,56 @@ import pandas as pd
 
 def get_crypto(symbol="BTCUSDT"):
 
-    url = (
-        f"https://api.binance.com/api/v3/klines"
-        f"?symbol={symbol}&interval=1m&limit=100"
-    )
+    try:
 
-    response = requests.get(url)
+        url = (
+            f"https://api.binance.com/api/v3/klines"
+            f"?symbol={symbol}&interval=1m&limit=100"
+        )
 
-    data = response.json()
+        response = requests.get(url, timeout=10)
 
-    # ❌ erreur API
-    if not isinstance(data, list):
+        data = response.json()
+
+        # ❌ erreur API
+        if not isinstance(data, list):
+            print("BINANCE ERROR:", data)
+            return None
+
+        # 📊 dataframe
+        df = pd.DataFrame(data, columns=[
+
+            "time",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+
+            "close_time",
+            "quote_asset_volume",
+            "trades",
+            "taker_buy_base",
+            "taker_buy_quote",
+            "ignore"
+        ])
+
+        # 🔢 convertir nombres
+        numeric_cols = [
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume"
+        ]
+
+        for col in numeric_cols:
+            df[col] = pd.to_numeric(df[col])
+
+        return df
+
+    except Exception as e:
+
+        print("MARKET ERROR:", e)
+
         return None
-
-    # 📊 dataframe
-    df = pd.DataFrame(data, columns=[
-
-        "time",
-        "open",
-        "high",
-        "low",
-        "close",
-        "volume",
-
-        "close_time",
-        "quote_asset_volume",
-        "trades",
-        "taker_buy_base",
-        "taker_buy_quote",
-        "ignore"
-    ])
-
-    # 🔢 convertir nombres
-    numeric_cols = [
-        "open",
-        "high",
-        "low",
-        "close",
-        "volume"
-    ]
-
-    for col in numeric_cols:
-        df[col] = pd.to_numeric(df[col])
-
-    return df
