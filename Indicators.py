6348603ca2f1a0dba44dfe05e8005ad95e50f635
Support Vimba boss <moto34091@gmail.com>
2026-05-21 08:58:53 +0200

    Update Indicators.py

--- Indicators.py
+++ Indicators.py
@@ -1,8 +1,24 @@
 from ta.trend import EMAIndicator
 from ta.momentum import RSIIndicator
 
+
 def add_indicators(df):
-    df["ema5"] = EMAIndicator(df["close"], window=5).ema_indicator()
-    df["ema13"] = EMAIndicator(df["close"], window=13).ema_indicator()
-    df["rsi"] = RSIIndicator(df["close"], window=7).rsi()
+
+    # 📈 EMA
+    df["ema5"] = EMAIndicator(
+        close=df["close"],
+        window=5
+    ).ema_indicator()
+
+    df["ema13"] = EMAIndicator(
+        close=df["close"],
+        window=13
+    ).ema_indicator()
+
+    # 📊 RSI
+    df["rsi"] = RSIIndicator(
+        close=df["close"],
+        window=7
+    ).rsi()
+
     return df
